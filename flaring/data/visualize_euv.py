f86dcc99aa3d8518c2778a9cff7da14c027c9d1c
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-07-23 16:53:25 +0200

    remove comment lines

--- flaring/data/visualize_euv.py
+++ flaring/data/visualize_euv.py
@@ -1,16 +1,12 @@
-import os
 import glob
-import numpy as np
-from datetime import datetime
-from skimage.transform import resize
-from matplotlib import colormaps as cm
-from tqdm import tqdm
-from multiprocessing import Pool
-from PIL import Image
-import sunpy.visualization.colormaps
-import matplotlib.pyplot as plt
-import csv
+import os
 import warnings
+from multiprocessing import Pool
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
 warnings.filterwarnings("ignore")
 
 data_dir = "/mnt/data2/AIA_processed"
@@ -25,7 +21,7 @@ for wl in wavelengths:
 npy_files = sorted(glob.glob(f"{data_dir}/*.npy"))
 print(f"Found {len(npy_files)} .npy files.")
 
-## Function to remove corrupted files with os.remove
+
 def remove_np_file(filepath):
     """
     Remove a .npy file if it is corrupted or cannot be loaded.
@@ -50,7 +46,7 @@ def remove_np_file(filepath):
             print(f"Failed to delete file: {delete_error}")
         return None
 
-## Process single file
+
 def process_file(fpath):
     """
     Process a single .npy file, visualize the AIA frames, and save them as images.
@@ -63,19 +59,21 @@ def process_file(fpath):
 
     """
     file = np.load(fpath)
-    fig,ax = plt.subplots(1,1,figsize = (10, 10), dpi=300)
+    fig, ax = plt.subplots(1, 1, figsize=(10, 10), dpi=300)
     for ch in range(len(wavelengths)):
         wl = wavelengths[ch]
-        #out_path = os.path.join(output_base, wl, f"aia_{wl}_{fpath.split('.')[0]}")
+        # out_path = os.path.join(output_base, wl, f"aia_{wl}_{fpath.split('.')[0]}")
         # Apply colormap and convert to RGB
         cmap = plt.get_cmap(f'sdoaia{wl}')
-        plt.imshow(file[ch, :, :], cmap = cmap, origin ="lower", vmax =1, vmin = -1)
-        plt.title(fpath.split('.')[0].split('/')[4], fontsize = 15)
-        plt.savefig(os.path.join(output_base, str(wl), fpath.split('.')[0].split('/')[4])+ ".jpg") ## Modified the code logic
+        plt.imshow(file[ch, :, :], cmap=cmap, origin="lower", vmax=1, vmin=-1)
+        plt.title(fpath.split('.')[0].split('/')[4], fontsize=15)
+        plt.savefig(
+            os.path.join(output_base, str(wl), fpath.split('.')[0].split('/')[4]) + ".jpg")  ## Modified the code logic
     plt.close()
-## Main function
+
+
 if __name__ == "__main__":
-    #for i in tqdm(range(53314, len(npy_files))):
+    # for i in tqdm(range(53314, len(npy_files))):
     #    npy_files[i] = remove_np_file(npy_files[i])
     with Pool(processes=90) as pool:
         # Remove corrupted files
