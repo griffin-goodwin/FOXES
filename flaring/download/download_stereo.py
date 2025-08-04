1985d747493d8a162a1ca189c447aac5637b2bef
christoph schirninger <christoph.schirninger@uni-graz.at>
2025-08-04 16:52:04 +0200

    fix stereo downloader and remove 304 channel

--- flaring/download/download_stereo.py
+++ flaring/download/download_stereo.py
@@ -4,15 +4,12 @@ import os
 import shutil
 from datetime import timedelta, datetime
 from glob import glob
-from multiprocessing import Pool
-from random import sample
 
 import drms
+import pandas as pd
 from astropy import units as u
-from dateutil.relativedelta import relativedelta
 from sunpy.map import Map
 from sunpy.net import Fido, attrs as a
-from tqdm import tqdm
 
 
 class STEREODownloader:
@@ -22,13 +19,14 @@ class STEREODownloader:
     Args:
         base_path (str): Path to the directory where the downloaded data should be stored.
     """
+
     def __init__(self, base_path):
         self.base_path = base_path
-        self.wavelengths = [304, 284, 195, 171]
-        self.dirs = ['304', '284', '195', '171']
+        self.wavelengths = [284, 195, 171]
+        self.dirs = ['284', '195', '171']
         [os.makedirs(os.path.join(base_path, dir), exist_ok=True) for dir in self.dirs]
 
-    def downloadDate(self, sample):
+    def downloadDate(self, date):
         """
         Download the data for the given date.
 
@@ -38,18 +36,18 @@ class STEREODownloader:
         Returns:
             list: List of paths to the downloaded files.
         """
-        date, source = sample
+
         files = []
         try:
             # Download SECCHI
             for wl in self.wavelengths:
-                files += [self.downloadSECCHI(date, wl, source)]
+                files += [self.downloadSECCHI(date, wl)]
             logging.info('Download complete %s' % date.isoformat())
         except Exception as ex:
             logging.error('Unable to download %s: %s' % (date.isoformat(), str(ex)))
             [os.remove(f) for f in files if os.path.exists(f)]
 
-    def downloadSECCHI(self, query_date, wl, source):
+    def downloadSECCHI(self, query_date, wl):
         """
         Download the SECCHI data for the given date, wavelength and source.
 
@@ -65,10 +63,10 @@ class STEREODownloader:
         if os.path.exists(file_path):
             return file_path  # skip existing downloads (e.g. retry)
         #
-        search = Fido.search(a.Time(query_date - timedelta(minutes=15), query_date + timedelta(minutes=15)),
-                             a.Provider('SSC'), a.Source(source), a.Instrument('SECCHI'), a.Wavelength(wl * u.AA))
+        search = Fido.search(a.Time(query_date - timedelta(minutes=2), query_date + timedelta(minutes=2)),
+                             a.Provider('SSC'), a.Instrument('SECCHI'), a.Wavelength(wl * u.AA))
         assert search.file_num > 0, "No data found for %s (%s)" % (query_date.isoformat(), wl)
-        search = sorted(search['vso'], key=lambda x: abs(x['Start Time'].datetime - query_date).total_seconds())
+        search = sorted(search['vso'], key=lambda x: abs(x['Start Time'].datetime.item() - query_date).total_seconds())
         for entry in search:
             files = Fido.fetch(entry, path=self.base_path, progress=False)
             if len(files) != 1:
@@ -92,8 +90,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Download STEREO data')
     parser.add_argument('--download_dir', type=str, help='path to the download directory.')
     parser.add_argument('--n_workers', type=str, help='number of parallel threads.', required=False, default=4)
-    parser.add_argument('--start_date', type=str, help='start date for the download.', required=False, default='2008-05-01')
-    parser.add_argument('--end_date', type=str, help='end date for the download.', required=False, default=str(datetime.now()).split(' ')[0])
+    parser.add_argument('--start_date', type=str, help='start date for the download.', required=False,
+                        default='2008-05-01')
+    parser.add_argument('--end_date', type=str, help='end date for the download.', required=False,
+                        default=str(datetime.now()).split(' ')[0])
     parser.add_argument('--cadence', type=int, help='cadence for the download.', required=False, default=60)
 
     args = parser.parse_args()
@@ -103,7 +103,7 @@ if __name__ == '__main__':
     end_date = args.end_date
     cadence = args.cadence
 
-    drms_client = drms.Client(email='robert.jarolim@uni-graz.at', verbose=False)
+    drms_client = drms.Client(email='robert.jarolim@uni-graz.at')
     download_util = STEREODownloader(base_path)
     logging.basicConfig(
         level=logging.INFO,
@@ -112,26 +112,13 @@ if __name__ == '__main__':
         ])
 
     existing_dates = set(
-        [os.path.basename(f).replace('.fits', '')[:10] for f in glob('/mnt/data/STEREO-EUVI/**/*.fits')])
+        [os.path.basename(f).replace('.fits', '')[:10] for f in glob('/mnt/data2/STEREO-EUVI/**/*.fits')])
 
     start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
     end_date_datetime = datetime.strptime(end_date, "%Y-%m-%d")
-    num_months = (end_date_datetime.year - start_date_datetime.year) * 12 + (end_date_datetime.month - start_date_datetime.month)
-    month_dates = [start_date_datetime + relativedelta(months=i) for i in range(num_months)]
-    for date in month_dates:
-        samples = []
-        for i in range(((date + relativedelta(months=1)) - date).days):
-            search_date = date + timedelta(days=i)
-            if '%04d-%02d-%02d' % (search_date.year, search_date.month, search_date.day) in existing_dates:
-                continue
-            search = Fido.search(a.Time(search_date, search_date + timedelta(minutes=cadence * u.minute)),
-                                 a.Provider('SSC'), a.Instrument('SECCHI'), a.Wavelength(171 * u.AA))
-            if search.file_num == 0:
-                continue
-            dates = search['vso']['Start Time']
-            sources = search['vso']['Source']
-            samples += [sample([(d.datetime, s) for d, s in zip(dates, sources)], 1)[0]]
+    # num_months = (end_date_datetime.year - start_date_datetime.year) * 12 + (end_date_datetime.month - start_date_datetime.month)
+    # month_dates = [start_date_datetime + relativedelta(months=i) for i in range(num_months)]
 
-        logging.info("TOTAL DAYS (%s): %d" % (date.isoformat(), len(samples)))
-        with Pool(n_workers) as p:
-            [None for _ in tqdm(p.imap_unordered(download_util.downloadDate, samples), total=len(samples))]
+    for d in [start_date_datetime + i * timedelta(minutes=3) for i in
+              range((end_date_datetime - start_date_datetime) // timedelta(minutes=3))]:
+        download_util.downloadDate(d)
