31df76b2b5cc859cd4f2b181d30b376fb44de9cf
griffin-goodwin <ggoodwin5@gsu.edu>
2025-09-03 12:15:42 -0400

    update model configuration: adjust batch size, patch size, dropout rate, and checkpoint paths

--- flaring/forecasting/training/train.py
+++ flaring/forecasting/training/train.py
@@ -166,7 +166,7 @@ checkpoint_callback = ModelCheckpoint(
     dirpath=config_data['data']['checkpoints_dir'],
     monitor='val_total_loss',
     mode='min',
-    save_top_k=4,
+    save_top_k=10,
     filename=f"{config_data['wandb']['wb_name']}-{{epoch:02d}}-{{val_total_loss:.4f}}"
 )
 
