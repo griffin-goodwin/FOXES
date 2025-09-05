5d46a29a3148d7628d6715f07fa047d498e3df45
Griffin Goodwin <ggoodwin5@gsu.edu>
2025-09-05 16:10:48 +0000

    updates to base model

--- forecasting/models/base_model.py
+++ forecasting/models/base_model.py
@@ -2,30 +2,18 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from pytorch_lightning import LightningModule
-from collections import deque
-import numpy as np
-
-# Import adaptive loss and normalization functions
-from .vit_patch_model import SXRRegressionDynamicLoss, normalize_sxr, unnormalize_sxr
 
 class BaseModel(LightningModule):
-    def __init__(self, model, loss_func, lr, sxr_norm=None, weight_decay=1e-5, 
+    def __init__(self, model, loss_func, lr, weight_decay=1e-5, 
                  cosine_restart_T0=50, cosine_restart_Tmult=2, cosine_eta_min=1e-7):
         super().__init__()
         self.model = model
         self.loss_func = loss_func
-        self.lr = lr
-        self.sxr_norm = sxr_norm
-        self.weight_decay = weight_decay
-        self.cosine_restart_T0 = cosine_restart_T0
-        self.cosine_restart_Tmult = cosine_restart_Tmult
-        self.cosine_eta_min = cosine_eta_min
-        
-        # Initialize adaptive loss if sxr_norm is provided
-        if sxr_norm is not None:
-            self.adaptive_loss = SXRRegressionDynamicLoss(window_size=1500)
-        else:
-            self.adaptive_loss = None
+        self.lr = float(lr)
+        self.weight_decay = float(weight_decay)
+        self.cosine_restart_T0 = int(cosine_restart_T0)
+        self.cosine_restart_Tmult = int(cosine_restart_Tmult)
+        self.cosine_eta_min = float(cosine_eta_min)
 
     def forward(self, x):
         return self.model(x)
@@ -55,19 +43,9 @@ class BaseModel(LightningModule):
     def training_step(self, batch, batch_idx):
         x, target = batch
         pred = self(x)
+        loss = self.loss_func(torch.squeeze(pred), target)
         
-        # Use adaptive loss if available and sxr_norm is provided
-        if self.adaptive_loss is not None and self.sxr_norm is not None:
-            raw_preds_squeezed = torch.squeeze(pred)
-            target_un = unnormalize_sxr(target, self.sxr_norm)
-            norm_preds_squeezed = normalize_sxr(raw_preds_squeezed, self.sxr_norm)
-            loss, weights = self.adaptive_loss.calculate_loss(
-                norm_preds_squeezed, target, target_un, raw_preds_squeezed
-            )
-        else:
-            loss = self.loss_func(torch.squeeze(pred), target)
-        
-        self.log('train_loss', loss)
+        self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         current_lr = self.trainer.optimizers[0].param_groups[0]['lr']
         self.log('learning_rate', current_lr, on_step=True, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         return loss
@@ -75,35 +53,15 @@ class BaseModel(LightningModule):
     def validation_step(self, batch, batch_idx):
         x, target = batch
         pred = self(x)
+        loss = self.loss_func(torch.squeeze(pred), target)
         
-        # Use adaptive loss if available and sxr_norm is provided
-        if self.adaptive_loss is not None and self.sxr_norm is not None:
-            raw_preds_squeezed = torch.squeeze(pred)
-            target_un = unnormalize_sxr(target, self.sxr_norm)
-            norm_preds_squeezed = normalize_sxr(raw_preds_squeezed, self.sxr_norm)
-            loss, weights = self.adaptive_loss.calculate_loss(
-                norm_preds_squeezed, target, target_un, raw_preds_squeezed
-            )
-        else:
-            loss = self.loss_func(torch.squeeze(pred), target)
-        
-        self.log('val_loss', loss)
+        self.log('val_total_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         return loss
 
     def test_step(self, batch, batch_idx):
         x, target = batch
         pred = self(x)
+        loss = self.loss_func(torch.squeeze(pred), target)
         
-        # Use adaptive loss if available and sxr_norm is provided
-        if self.adaptive_loss is not None and self.sxr_norm is not None:
-            raw_preds_squeezed = torch.squeeze(pred)
-            target_un = unnormalize_sxr(target, self.sxr_norm)
-            norm_preds_squeezed = normalize_sxr(raw_preds_squeezed, self.sxr_norm)
-            loss, weights = self.adaptive_loss.calculate_loss(
-                norm_preds_squeezed, target, target_un, raw_preds_squeezed
-            )
-        else:
-            loss = self.loss_func(torch.squeeze(pred), target)
-        
-        self.log('test_loss', loss)
+        self.log('test_loss', loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
         return loss
--- forecasting/models/linear_and_hybrid.py
+++ forecasting/models/linear_and_hybrid.py
@@ -10,14 +10,14 @@ from forecasting.models.base_model import BaseModel
 from torchvision.models import resnet18
 
 class LinearIrradianceModel(BaseModel):
-    def __init__(self, d_input, d_output, loss_func=HuberLoss(), lr=1e-4, sxr_norm=None, 
+    def __init__(self, d_input, d_output, loss_func=HuberLoss(), lr=1e-4, 
                  weight_decay=1e-5, cosine_restart_T0=50, cosine_restart_Tmult=2, cosine_eta_min=1e-7):
         self.n_channels = d_input
         self.outSize = d_output
         model = nn.Linear(2 * self.n_channels, self.outSize)
-        super().__init__(model=model, loss_func=loss_func, lr=lr, sxr_norm=sxr_norm,
-                        weight_decay=weight_decay, cosine_restart_T0=cosine_restart_T0,
-                        cosine_restart_Tmult=cosine_restart_Tmult, cosine_eta_min=cosine_eta_min)
+        super().__init__(model=model, loss_func=loss_func, lr=float(lr),
+                        weight_decay=float(weight_decay), cosine_restart_T0=int(cosine_restart_T0),
+                        cosine_restart_Tmult=int(cosine_restart_Tmult), cosine_eta_min=float(cosine_eta_min))
 
     def forward(self, x, **kwargs):
 
@@ -52,20 +52,20 @@ class LinearIrradianceModel(BaseModel):
 
 class HybridIrradianceModel(BaseModel):
     def __init__(self, d_input, d_output, cnn_model='resnet', ln_model=True, ln_params=None, lr=1e-4, cnn_dp=0.75, loss_func=HuberLoss(),
-                 sxr_norm=None, weight_decay=1e-5, cosine_restart_T0=50, cosine_restart_Tmult=2, cosine_eta_min=1e-7):
-        super().__init__(model=None, loss_func=loss_func, lr=lr, sxr_norm=sxr_norm,
-                        weight_decay=weight_decay, cosine_restart_T0=cosine_restart_T0,
-                        cosine_restart_Tmult=cosine_restart_Tmult, cosine_eta_min=cosine_eta_min)
+                 weight_decay=1e-5, cosine_restart_T0=50, cosine_restart_Tmult=2, cosine_eta_min=1e-7):
+        super().__init__(model=None, loss_func=loss_func, lr=float(lr),
+                        weight_decay=float(weight_decay), cosine_restart_T0=int(cosine_restart_T0),
+                        cosine_restart_Tmult=int(cosine_restart_Tmult), cosine_eta_min=float(cosine_eta_min))
         self.n_channels = d_input
         self.outSize = d_output
         self.ln_params = ln_params
         self.ln_model = None
         if ln_model:
-            self.ln_model = LinearIrradianceModel(d_input, d_output, loss_func=loss_func, lr=lr,
-                                                sxr_norm=sxr_norm, weight_decay=weight_decay,
-                                                cosine_restart_T0=cosine_restart_T0,
-                                                cosine_restart_Tmult=cosine_restart_Tmult,
-                                                cosine_eta_min=cosine_eta_min)
+            self.ln_model = LinearIrradianceModel(d_input, d_output, loss_func=loss_func, lr=float(lr),
+                                                weight_decay=float(weight_decay),
+                                                cosine_restart_T0=int(cosine_restart_T0),
+                                                cosine_restart_Tmult=int(cosine_restart_Tmult),
+                                                cosine_eta_min=float(cosine_eta_min))
         if self.ln_params is not None and self.ln_model is not None:
             self.ln_model.model.weight = nn.Parameter(self.ln_params['weight'])
             self.ln_model.model.bias = nn.Parameter(self.ln_params['bias'])
