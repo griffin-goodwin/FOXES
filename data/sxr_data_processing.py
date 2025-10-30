b6d3638f78f68a1b7915986137215c3200bc5951
Griffin Goodwin <ggoodwin5@gsu.edu>
2025-10-30 14:39:38 +0000

    made changes to directory locations... removed some unnecessary files

--- data/sxr_data_processing.py
+++ data/sxr_data_processing.py
@@ -22,7 +22,7 @@ class SXRDataProcessor:
         Directory where combined GOES data will be saved.
     """
 
-    def __init__(self, data_dir: str = '/mnt/data/AUGUST/GOES-timespan', output_dir: str = '/mnt/data/AUGUST/combined'):
+    def __init__(self, data_dir: str = '/mnt/data/PAPER/GOES-timespan', output_dir: str = '/mnt/data/PAPER/GOES-timespan/combined'):
         self.data_dir = Path(data_dir)
         self.output_dir = Path(output_dir)
         self.output_dir.mkdir(exist_ok=True)
@@ -198,9 +198,9 @@ class SXRDataProcessor:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess GOES X-ray data.')
-    parser.add_argument('--data_dir', type=str, default='/mnt/data/AUGUST/GOES-timespan',
+    parser.add_argument('--data_dir', type=str, default='/mnt/data/PAPER/GOES-timespan',
                         help='Directory where downloaded GOES data is stored.')
-    parser.add_argument('--output_dir', type=str, default='/mnt/data/AUGUST/combined',
+    parser.add_argument('--output_dir', type=str, default='/mnt/data/PAPER/GOES-timespan/combined',
                         help='Directory where combined GOES data will be saved.')
     args = parser.parse_args()
     
